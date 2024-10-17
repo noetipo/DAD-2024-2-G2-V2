739b567d46c10bbf52dc2ef85d04e0224a1c6c39
dmamanipar@unsa.edu.pe <noe.tipo@gmail.com>
2024-10-17 08:41:44 -0500

    filtros de seguridad

---
+++ ms-gateway-server/src/main/java/upeu/edu/pe/msgatewayserver/dto/TokenDto.java
@@ -0,0 +1,19 @@
+package upeu.edu.pe.msgatewayserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+
+@NoArgsConstructor
+
+
+@Builder
+@AllArgsConstructor
+@Data
+public class TokenDto {
+    private String token;
+}
